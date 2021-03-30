cf9dc198a0b9235e4cab7d7f831c98178420a8fb
tk6996 <watcharinkanha@gmail.com>
2021-03-30 13:18:39 +0700

    update

--- deployjob.bat
+++
@@ -1,9 +0,0 @@
-cd D:\Users\Administrator\Desktop\Dino-boba
-ng build --prod --output-path dist --base-href "https://tk6996.github.io/toc-dino-pizza-web/" && cd ./dist
-copy index.html 404.html
-git init
-git remote add origin https://github.com/tk6996/toc-dino-pizza-web.git
-git pull --rebase origin main
-git add .
-git commit -m "update"
-git push origin main
